079e4c702a309dc8cbcf10434117fe9af9f686d6
Indrajeet2002 <Indrajeet.patwardhan@gmail.com>
2022-02-26 13:34:06 -0800

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HighlightIcon from '@mui/icons-material/Add';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        FitNotes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
